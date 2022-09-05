df319682adea6926621d3714e55c3097c78bf4df
HongSeok Choi <hongking9@gist.ac.kr>
2022-09-05 20:42:38 +0900

    Add files via upload

---
+++ bus-stop-keras/download_params.py
@@ -0,0 +1,10 @@
+"""
+First author: HongSeok Choi (hongking9@gist.ac.kr)
+Corresponding author: Hyunju Lee (hyunjulee@gist.ac.kr)
+Code for Early Stopping Based on Unlabeled Samples in Text Classification.
+"""
+import requests
+
+URL = "https://cdn.huggingface.co/bert-base-uncased-pytorch_model.bin"
+response = requests.get(URL)
+open("./params/bert_base/pytorch_model.bin","wb").write(response.content)
